1fbd52affeb8cbaced5ee0c08a8ce57053214c77
jackie saltalepre <pirrulez@gmail.com>
2019-07-10 17:38:05 +0100

    merged images and metadata, cleaned up, mousewheel

--- aaa.py
+++ aaa.py
@@ -2,12 +2,13 @@
 # coding: utf-8
 
 """ 
+	NEXT UP:
 	dynamically enlarge/shrink map
-	tag metadata (e.g. WALKABLE, OBSTACLE, etc.)
-	potentially let user add metadata only (over existing tiles)
+
+	TODO:
+	potentially allow for manual metadata override
 	tile randomiser among tileset
-	specify tileset and tile in metadata - at the moment it does not differentiate!
-	change input to improve experience, potentially add joystick support
+	NOTE: no sanity checks at all on metadata files for now
 """
 
 import pygame
@@ -56,20 +57,38 @@ class Util:
 
 	def initialise_tilesets(self):
 		tilesets = list()
-
 		folder_path = os.path.dirname(__file__)
-		target_path = "tilesets"
+		target_path = 'tilesets'
 		tilesets_path = os.path.join(folder_path, target_path)
 
 		subfolders = [x[0] for x in os.walk(tilesets_path)]
 
 		for subfolder in subfolders:
+
+			# Skips the current folder
+			if subfolder == tilesets_path:
+				continue
+
 			tileset = list()
-			tiles = [tile for tile in os.listdir(subfolder) if tile[-4:] == '.jpg']
-			for tile in tiles:
-				tileset.append(pygame.image.load(os.path.join(subfolder, tile)))
-			if tileset:
-				tilesets.append(tileset)
+
+			try:
+				with open(os.path.join(subfolder, 'metadata.txt'), 'r') as metadata_file:
+
+					for line in metadata_file:
+						text = line.split()
+
+						# Avoids empty lines
+						if text:
+							tileset.append(text)
+						
+			except IOError:
+				print('Error: no metadata file for tileset {}'.format(subfolder))
+
+			# Replaces the file names with the actual images
+			for entry in tileset:
+				entry[0] = pygame.image.load(os.path.join(subfolder, entry[0]))
+
+			tilesets.append(tileset)
 
 		return tilesets
 
@@ -78,14 +97,22 @@ class Util:
 		img_path = os.path.join(folder_path, "blank.jpg")
 		return pygame.image.load(img_path)
 
+	def quit(self):
+		pygame.quit()
+		sys.exit()
+
+	def is_coord_within_map_bounds(self, coord):
+		if coord[0] < MapEditor.MAPWIDTH and coord[1] < MapEditor.MAPHEIGHT:
+			return True
+		return False
 
 
 class MapEditor:
 	TILESIZE = 40
-	SCREENWIDTH = TILESIZE * 10
-	SCREENHEIGHT = TILESIZE * 10
-	MAPWIDTH = TILESIZE * 15
-	MAPHEIGHT = TILESIZE * 15
+	SCREENWIDTH = TILESIZE * 12
+	SCREENHEIGHT = TILESIZE * 12
+	MAPWIDTH = TILESIZE * 40
+	MAPHEIGHT = TILESIZE * 40
 	FPS = 90
 
 	def __init__(self):
@@ -96,59 +123,94 @@ class MapEditor:
 		self.util = Util()
 		
 		self.my_map = pygame.Surface((MapEditor.MAPWIDTH, MapEditor.MAPHEIGHT))
-		self.metadata = [[0] * (self.my_map.get_width() / MapEditor.TILESIZE) for i in range(self.my_map.get_height() / MapEditor.TILESIZE)]
+		self.my_metadata = [[0] * (self.my_map.get_width() / MapEditor.TILESIZE) for i in range(self.my_map.get_height() / MapEditor.TILESIZE)]
 
 		self.tilesets = self.util.initialise_tilesets()
 		self.curr_tileset_index = 0
 		self.curr_tileset = self.tilesets[self.curr_tileset_index]
 		self.curr_tile_index = 0
-		self.curr_tile = self.curr_tileset[self.curr_tile_index]
+		self.curr_tile = self.curr_tileset[self.curr_tile_index][0]
+		
 		self.blank_tile = self.util.initialise_blank_tile()
 
 		self.camera = Camera(self.screen.get_size(), self.my_map.get_size())
 		self.clock = pygame.time.Clock()
 
+	def next_tileset(self):
+		self.curr_tileset_index = (self.curr_tileset_index + 1) % len(self.tilesets)
+		self.curr_tileset = self.tilesets[self.curr_tileset_index]
+
+		# Reset index to 0 in case new tileset is smaller than previous one - avoids index out of range
+		self.curr_tile_index = 0
+		self.curr_tile = self.curr_tileset[self.curr_tile_index][0]
+
+	def prev_tileset(self):
+		self.curr_tileset_index = (self.curr_tileset_index - 1) % len(self.tilesets)
+		self.curr_tileset = self.tilesets[self.curr_tileset_index]
+
+		# Reset index to 0 in case new tileset is smaller than previous one - avoids index out of range
+		self.curr_tile_index = 0
+		self.curr_tile = self.curr_tileset[self.curr_tile_index][0]
+
+	def next_tile(self):
+		self.curr_tile_index = (self.curr_tile_index + 1) % len(self.curr_tileset)
+		self.curr_tile = self.curr_tileset[self.curr_tile_index][0]
+
+	def prev_tile(self):
+		self.curr_tile_index = (self.curr_tile_index - 1) % len(self.curr_tileset)
+		self.curr_tile = self.curr_tileset[self.curr_tile_index][0]
+
+	def save(self):
+		pygame.image.save(self.my_map, os.path.join(os.path.dirname(__file__), 'map.jpeg'))
+		with open(os.path.join(os.path.dirname(__file__), 'metadata.p'), 'wb') as out_file:
+			pickle.dump(self.my_metadata, out_file)
+
+	def load(self):
+		self.my_map = pygame.image.load(os.path.join(os.path.dirname(__file__), 'map.jpeg'))
+		with open(os.path.join(os.path.dirname(__file__), 'metadata.p'), 'rb') as in_file:
+			self.my_metadata = pickle.load(in_file)
+
+	def clear(self):
+		self.my_map = pygame.Surface((MapEditor.MAPWIDTH, MapEditor.MAPHEIGHT))
+		self.metadata = [[0] * (self.my_map.get_width() / MapEditor.TILESIZE) for i in range(self.my_map.get_height() / MapEditor.TILESIZE)]
+
 	def process_events(self):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
-				pygame.quit()
-				sys.exit()
+				self.util.quit()
 
 			elif event.type == pygame.KEYDOWN:
 				# ESC - exit program
 				if event.key == pygame.K_ESCAPE:
-					pygame.quit()
-					sys.exit()
+					self.util.quit()
 
-				# 1 - change tile
-				elif event.key == pygame.K_1:
-					self.curr_tile_index = (self.curr_tile_index + 1) % len(self.curr_tileset)
-					self.curr_tile = self.curr_tileset[self.curr_tile_index]
+				# E - next tileset
+				elif event.key == pygame.K_e:
+					self.next_tileset()
 
-				# 2 - change tileset
-				elif event.key == pygame.K_2:
-					self.curr_tileset_index = (self.curr_tileset_index + 1) % len(self.tilesets)
-					self.curr_tileset = self.tilesets[self.curr_tileset_index]
-
-					# Reset index to 0 in case new tileset is smaller than previous one - avoids index out of range
-					self.curr_tile = self.curr_tileset[0]
+				# Q - prev tileset
+				elif event.key == pygame.K_q:
+					self.prev_tileset()
 
 				# K - save
 				elif event.key == pygame.K_k:
-					pygame.image.save(self.my_map, 'map.jpeg')
-					with open('metadata.p', 'wb') as out_file:
-						pickle.dump(self.metadata, out_file)
+					self.save()
 
 				# L - load
 				elif event.key == pygame.K_l:
-					self.my_map = pygame.image.load('map.jpeg')
-					with open('metadata.p', 'rb') as in_file:
-						self.metadata = pickle.load(in_file)
+					self.load()
 
 				# C - clear
 				elif event.key == pygame.K_c:
-					self.my_map = pygame.Surface((MapEditor.MAPWIDTH, MapEditor.MAPHEIGHT))
-					self.metadata = [[0] * (self.my_map.get_width() / MapEditor.TILESIZE) for i in range(self.my_map.get_height() / MapEditor.TILESIZE)]
+					self.clear()
+
+			# Change tiles with mousewheel
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				if event.button == 4:
+					self.prev_tile()
+				elif event.button == 5:
+					self.next_tile()
+
 
 	def get_camera_movements(self):
 		# WASD - move camera
@@ -162,33 +224,28 @@ class MapEditor:
 		if keys[pygame.K_d]:
 			self.camera.move(1, 0)
 
-	def is_coord_within_map_bounds(self, coord):
-		if coord[0] < MapEditor.MAPWIDTH and coord[1] < MapEditor.MAPHEIGHT:
-			return True
-		return False
-
 	def process_mouse_clicks(self):
 		# Left click - draw
 		if pygame.mouse.get_pressed()[0]:
 			coord = self.util.round_down_coords(self.camera.apply_to_coord(list(pygame.mouse.get_pos())))
 
-			if not self.is_coord_within_map_bounds(coord):
+			if not self.util.is_coord_within_map_bounds(coord):
 				return
 
 			self.my_map.blit(self.curr_tile, coord)
 			x, y = self.util.coord_to_index(coord)
-			self.metadata[y][x] = self.curr_tile_index
+			self.my_metadata[y][x] = self.curr_tileset[self.curr_tile_index][1:]
 
 		# Right click - erase
 		elif pygame.mouse.get_pressed()[2]:
 			coord = self.util.round_down_coords(self.camera.apply_to_coord(list(pygame.mouse.get_pos())))
 
-			if not self.is_coord_within_map_bounds(coord):
+			if not self.util.is_coord_within_map_bounds(coord):
 				return
 
 			self.my_map.blit(self.blank_tile, coord)
 			x, y = self.util.coord_to_index(coord)
-			self.metadata[y][x] = 0
+			self.my_metadata[y][x] = [0]
 
 	def draw(self):
 		# Draw map
@@ -196,7 +253,7 @@ class MapEditor:
 
 		# Preview current tile at cursor position
 		coord = self.util.round_down_coords(list(pygame.mouse.get_pos()))
-		if self.is_coord_within_map_bounds(coord):
+		if self.util.is_coord_within_map_bounds(coord):
 			self.screen.blit(self.curr_tile, coord)
 
 		pygame.display.flip()
